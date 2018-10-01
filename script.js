41116c24f7f61d7680be0ea7dc40ebf8f1b62757
LEI Haoyu <hylei@connect.hku.hk>
2018-10-01 17:37:09 +0800

    templates for each page, angularJS framework

---
+++ script.js
@@ -0,0 +1,22 @@
+var app = angular.module("homepage", ["ngRoute"]);
+app.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl: "main.htm"
+	})
+	.when("/info", {
+		templateUrl: "info.htm"
+	})
+	.when("/progress", {
+		templateUrl: "progress.htm"
+	})
+	.when("/documentation", {
+		templateUrl: "documentation.htm"
+	})
+	.when("/references", {
+		templateUrl: "references.htm"
+	})
+	.when("/contacts", {
+		templateUrl: "contact.htm"
+	});
+});
